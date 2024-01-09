c9eb5118656157451201fef9ca81c3ff2bb641b8
NaiveDeveloper01 <156093463+NaiveDeveloper01@users.noreply.github.com>
2024-01-09 19:21:45 +0530

    BlogAppStarter

---
+++ App/src/test/java/com/Socialblog/App/SocialBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Socialblog.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
